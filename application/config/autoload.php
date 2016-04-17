f8c095149cbb00f5a4d32e293b8259f9fc9d75a2
DedyPradana <dedy90.informatika@gmail.com>
2016-04-17 20:43:04 +0700

    conf libraries

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('templates', 'database', 'session', 'uri');
+$autoload['libraries'] = array('templates', 'database', 'session', 'uri', 'image_crud');
 
 /*
 | -------------------------------------------------------------------
